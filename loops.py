92b4901d637561552b628eb6962f7fd6bce9505c
Manoj <111341899+Manojkumar077@users.noreply.github.com>
2022-09-02 15:22:13 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,22 @@
+'''
+Loops : Loops are iterative conditions to traverse through all the elements in a structure, until the condition satisifies.
+
+There are 2 types of loops :
+1) For loop
+2) While loop
+
+For Loop : Iterates all the elements in a data structure and returns it to executed variable.
+ex:
+fruits = [apple,mango,kiwi,melon]
+for i in fruits:
+    print(i)
+
+While loop : Iterates all the elements in a data structure based on a given condition.
+             The elements are returned until the condition is TRUE.
+ex:
+i=0
+while(i<5):
+    print(i)
+    i+=1
+    
+'''
