0b3545b605f85e6739cd5f1cd248088df73bbb26
Manoj <111341899+Manojkumar077@users.noreply.github.com>
2022-09-02 15:15:47 +0530

    Create decission.py

---
+++ decission.py
@@ -0,0 +1,20 @@
+'''Decision making statements are those that decide whether a block of statements has to execute or not
+ based on a condition. It returns the true or false values based on the data given by the user,
+ Decision making statements are also called Conditional Statements.'''
+
+"One set of statements are exected if the condition is true or other set of statemts are executed if the condition is false."
+''' 
+Three types of conditional statements : 
+
+1)  If Statement :
+                 If statement is the simplest of decision making statements. It contains a condition and a block of statement
+    If the condition is True, interpreter executes the block of statements, else not.
+    
+2)  If-else statement: 
+                 If-else statement contains a condition and two block of statements: if-block and else-block. 
+    If the condition is True, interpreter executes if-block, or if the condition is False, interpreter executes else-block.
+    
+3)  Elif statements: 
+                 Elif statements are used when there are more than 2 conditions , 
+    For instance if a condition from the if part fails interpreter checks for the condition in elif part.  
+'''
